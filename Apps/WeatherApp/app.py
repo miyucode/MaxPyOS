b1d162db4573910af91239e68b4f04c26ce1096b
MiyuCode <streamlabsdonationtools@gmail.com>
2022-03-20 11:59:39 +0100

    feat: add a system for verify if a city exist or no

--- Apps/WeatherApp/app.py
+++ Apps/WeatherApp/app.py
@@ -10,19 +10,22 @@ def weatherapp():
 	def getWeather(canvas):
 			city = namecity.get()
 			api = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=06c921750b9a82d8f5d1294e1586276f"
-
-			json_data = requests.get(api).json()
-			condition = json_data['weather'][0]['main']
-			temp = int(json_data['main']['temp'] - 273.15)
-			min_temp = int(json_data['main']['temp_min'] - 273.15)
-			max_temp = int(json_data['main']['temp_max'] - 273.15)
-			pressure = json_data['main']['pressure']
-			humidity = json_data['main']['humidity']
-			wind = json_data['wind']['speed']
-			final_info = condition + "\n" + str(temp) + "°C"
-			final_data = "\n" + "Minimal temperature: " + str(min_temp) + "°C" + "\n" + "Maximal temperature: " + str(max_temp) + "°C" + "\n" + "Pression: " + str(pressure) + "\n" + "Humidity: " + str(humidity) + "\n"
-			label1.config(text=final_info)
-			label2.config(text=final_data)
+			try:
+				json_data = requests.get(api).json()
+				condition = json_data['weather'][0]['main']
+				temp = int(json_data['main']['temp'] - 273.15)
+				min_temp = int(json_data['main']['temp_min'] - 273.15)
+				max_temp = int(json_data['main']['temp_max'] - 273.15)
+				pressure = json_data['main']['pressure']
+				humidity = json_data['main']['humidity']
+				wind = json_data['wind']['speed']
+				final_info = condition + "\n" + str(temp) + "°C"
+				final_data = "\n" + "Minimal temperature: " + str(min_temp) + "°C" + "\n" + "Maximal temperature: " + str(max_temp) + "°C" + "\n" + "Pression: " + str(pressure) + "\n" + "Humidity: " + str(humidity) + "\n"
+				label1.config(text=final_info)
+				label2.config(text=final_data)
+			except:
+				label1.config(text="This city doesn't exist !")
+				label2.config(text="")
 
 	weatherapp = Tk()
 	weatherapp.title("MaxPyOS - Weather App")
