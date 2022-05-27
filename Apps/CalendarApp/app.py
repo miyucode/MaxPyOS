7a921a7d219e367c6234c7d2a6ed668ca795c5c3
MiyuCode <streamlabsdonationtools@gmail.com>
2022-05-27 22:09:01 +0200

    add Calendar App

---
+++ Apps/CalendarApp/app.py
@@ -0,0 +1,28 @@
+from tkinter import *
+from tkinter.ttk import *
+from tkinter import ttk
+
+from tkcalendar import *
+
+import tkinter.messagebox as mb
+
+import datetime
+
+def calendarapp():
+    calendarapp = Tk()
+    calendarapp.title("MaxPyOS - Calendar")
+    calendarapp.geometry("300x200")
+    calendarapp.minsize(300, 350)
+    calendarapp.maxsize(350, 250)
+    calendarapp.iconbitmap("Apps/CalendarApp/icons/calendar-icon.ico")
+
+    current_day = datetime.date.today().day
+    current_month = datetime.date.today().month
+    current_year = datetime.date.today().year
+
+    cal = Calendar(calendarapp, selectmode="day", year=current_year, month=current_month, day=current_day)
+    cal.pack(pady=20)
+
+    Label(calendarapp, text=f"Today is the {current_day}/{current_month}/{current_year} !", font=("Monospace", 14)).pack(pady=20)
+
+    calendarapp.mainloop()
