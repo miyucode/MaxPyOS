175f1e74a36c45c1b7dd5c9b30903a306be68334
MiyuCode <streamlabsdonationtools@gmail.com>
2022-05-27 21:29:16 +0200

    fix MaxPyOS.py

--- MaxPyOS.py
+++ MaxPyOS.py
@@ -1,8 +1,10 @@
 from System.Boot import Boot
-from UI.Login import Login
 from System.Restart.Restart import configure_start_command
 
+from UI.Login import Login
+
 from sys import platform
+from time import *
 
 import os
 
@@ -11,6 +13,18 @@ def main():
 
 	Boot.Boot()
 	Login.login()
+
+	username_file = open('System/Ressources/username.txt', 'r')
+	username = username_file.read()
+
+	password_file = open('System/Ressources/password.txt', 'r')
+	password = password_file.read()
+
+	password_file.close()
+	username_file.close()
+
+	print(f"[{strftime('%H:%M:%S')}]: Your username is \"{username}\" and your password is \"{password}\".")
+
 
 if __name__ == '__main__':
 	configure_start_command(main)
