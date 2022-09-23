6a7dfad72e44c8156cfe894f0be91741807e2b38
MiyuCode <streamlabsdonationtools@gmail.com>
2022-09-23 22:09:21 +0200

    Ajout d'un texte montrant la dernière information la plus importante dans le monde (elle sera mis à jour toutes les 2 semaines)

--- Apps/CalendarApp/app.py
+++ Apps/CalendarApp/app.py
@@ -11,7 +11,7 @@ import datetime
 def calendarapp():
     calendarapp = Tk()
     calendarapp.title("MaxPyOS - Calendar")
-    calendarapp.geometry("300x200")
+    calendarapp.geometry("350x220")
     calendarapp.minsize(300, 350)
     calendarapp.maxsize(350, 250)
     calendarapp.iconbitmap("Apps/CalendarApp/icons/calendar-icon.ico")
@@ -23,6 +23,7 @@ def calendarapp():
     cal = Calendar(calendarapp, selectmode="day", year=current_year, month=current_month, day=current_day)
     cal.pack(pady=20)
 
-    Label(calendarapp, text=f"Today is the {current_day}/{current_month}/{current_year} !", font=("Monospace", 14)).pack(pady=20)
+    Label(calendarapp, text=f"Today is the {current_month}/{current_day}/{current_year} !", font=("Monospace", 14)).pack(pady=20)
+    Label(calendarapp, text="Last event: 9/8/2022, \nDeath of Elizabeth II. Rest in Peace !", font=("Monospace", 15)).pack()
 
     calendarapp.mainloop()
